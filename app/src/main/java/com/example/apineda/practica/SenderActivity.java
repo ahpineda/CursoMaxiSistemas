d83d4ebbae78940dec78494ddfed3ef38faa214b
ahpineda <alejandrohectorpineda@gmail.com>
2018-04-16 21:18:48 -0300

    toast

--- app/src/main/java/com/example/apineda/practica/SenderActivity.java
+++ app/src/main/java/com/example/apineda/practica/SenderActivity.java
@@ -1,26 +1,21 @@
 package com.example.apineda.practica;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SenderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sender);
+
     }
+
     public void botonApretado(View view){
         Toast.makeText(this, "boton apretado", Toast.LENGTH_LONG).show();
     }
-    public void lanzar(View view) {
-        Intent intent = new Intent(this, SenderActivity.class);
-        startActivity(intent);}
-
-    }
+}
